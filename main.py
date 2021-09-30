01294629c98b5766d5ef0df014b5d147de60c0be
mehds-backup <oulmakks@gmail.com>
2021-09-30 16:54:42 +0100

    removed the main call

--- main.py
+++ main.py
@@ -4,13 +4,11 @@
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def fizzbuzz(number: int):
+    result = ""
+    if number % 3 == 0:
+        result += "fizz"
+    if number % 5 ==     0:
+        result += "buzz"
+    return result
 
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
