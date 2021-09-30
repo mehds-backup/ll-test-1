cb53e2aad9aba1baedbca002f46d2ec955bf12dc
mehds-backup <oulmakks@gmail.com>
2021-09-30 17:04:44 +0100

    trying to optimize for when n1 and n2 are multiples of each other

--- main.py
+++ main.py
@@ -4,10 +4,25 @@
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 
-def fizzbuzz(number: int) -> str:
+def fizzbuzz(number: int, n1: int, n2: int) -> str:
+    larger = max(n1, n2)
+    smaller = min(n1, n2)
+
     result = ""
-    if number % 3 == 0:
+    if larger % smaller == 0:
+        if number % larger == 0:
+            return "ungabunga"
+        elif number % smaller == 0:
+            return "unga"
+
+    if number % smaller == 0:
         result += "unga"
-    if number % 5 == 0:
+    if number % larger == 0:
         result += "bunga"
     return result
+
+
+if __name__ == "__main__":
+    print(fizzbuzz(15, 5, 3))
+
+    print(fizzbuzz(56, 14, 7))
